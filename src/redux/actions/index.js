108a2865aa6a0931e88e1f21633f31a3d4b52a3b
Thomas Boos Pegler <tboospegler@gmail.com>
2022-08-26 14:43:27 -0400

    iniciando projeto

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,15 @@
 // Coloque aqui suas actions
+export const PERSONAL_INFO = 'PERSONAL_INFO';
+export const WALLET_INFO = 'WALLET_INFO';
+
+const walletSubmmit = (wallet) => ({
+  type: WALLET_INFO,
+  payload: wallet,
+});
+
+const userSubmmit = (user) => ({
+  type: PERSONAL_INFO,
+  payload: user,
+});
+
+export { walletSubmmit, userSubmmit };
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { PERSONAL_INFO } from '../actions';
+
+const initialState = {
+  email: '', // string que armazena o email da pessoa usuária
+};
+
+const user = (state = initialState, action) => {
+  switch (action.type) {
+  case PERSONAL_INFO:
+    return { ...state, email: 'teste' };
+
+  default:
+    return state;
+  }
+};
+export default user;
